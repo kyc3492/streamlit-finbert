f60f4c1a77cb98caf83a23b69665f603cf70e7f8
kyc3492 <kyc3492@gmail.com>
2022-12-13 15:55:10 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+## Quick Start
+
+FinBert를 활용한 간단한 호재/악재 판독기입니다.
+
+`뉴스기사`를 `텍스트`로 입력하면, `호재, 악재, 모호`를 판독하고,
+
+`호재`와 `악재`의 경우 몇 퍼센트인지 알려줍니다.
+
+Thanks to @jinmyeongAN
+
+
+### 1. 코드 다운 및 실행
+
+```python
+$ git clone https://github.com/kyc3492/streamlit-finbert
+$ pip install -r requirements.txt
+$ streamlit run app.py
+```
+
+### 2. 웹 페이지 접속
+
+```
+주소창에 http://localhost:8501 입력
+혹은, 와이파이 사용시 외부기기(스마트폰 등)에서도 http://192.168.0.5:8501 주소로 이용가능
+```
+## 실행 화면
+
+추후 업데이트...
+
+### 투자는 본인의 선택으로 신중히!!
---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("snunlp/KR-FinBert-SC")
+model = AutoModelForSequenceClassification.from_pretrained("snunlp/KR-FinBert-SC")
+classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
+
+# 웹 페이지 제목
+st.title("호재/악재 판독기 🤓")
+# 웹 페이지 부제목
+st.subheader('뉴스 기사 제목을 올려주세요!')
+
+# 웹 페이지에 입력
+with st.form(key="form"):
+    sentence = st.text_input(label="기사 제목", placeholder="삼성전자, 2년 만에 인도 스마트폰 시장 점유율 1위 '왕좌 탈환'")
+    submit = st.form_submit_button("Go!")
+
+# 버튼을 눌렀을 때, 작동되는 코드
+if submit:
+    st.write("이 종목은... 🤓")
+
+    # 모델의 inference가 끝날 때까지 기다림
+    with st.spinner("오이 종목은... 🤓"):
+        # classifier 라는 이름의 딥러닝 모델사용.
+        # 원하는 모델로 변경할 수 있다.
+        results = classifier(sentence)[0]
+
+    label = results["label"]
+    score = results["score"]
+    
+    # 모델로부터 얻은 결과를 원하는 방식, 폰트, 배치대로 화면에 보여줌
+    # 나만의 디자인을 원한다면 아래 링크 참고
+    # https://docs.streamlit.io/library/api-reference/layout
+    col1, col2 = st.columns(2)
+    possibility = score * 100
+
+    if label == "positive": col1.metric("호재입니다! 추매각?", "😁", f"{possibility} %")
+    elif label == "negative": col1.metric("악재입니다ㅠㅠ 손절각?", "😭", f"{positive} %")
+    else: col1.metric("이건...저도 잘 모르겠어요... 다른 기사 있나요?", "😅")
